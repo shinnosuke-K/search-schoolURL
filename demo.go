0f585a61828c6252cff7459f18fcda2e6dd004f6
shinnosuke-K <shin.050195@gmail.com>
2019-05-31 10:57:24 +0900

    try google search api

---
+++ demo.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"search-schoolURL/env"
+
+	"google.golang.org/api/customsearch/v1"
+	"google.golang.org/api/googleapi/transport"
+)
+
+func customSearchMain() {
+	client := &http.Client{Transport: &transport.APIKey{Key: env.ApiKey}}
+
+	svc, err := customsearch.New(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := svc.Cse.List(env.Query).Cx(env.Cx).Num(5).Do()
+	//resp, err := svc.Cse.List(env.Query).Cx(env.Cx).Num(5).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, result := range resp.Items {
+		fmt.Printf("#%d: %s\n", i+1, result.Title)
+		fmt.Printf("\t%s\n", result.Snippet)
+	}
+}
+
+func main() {
+	customSearchMain()
+}
---
+++ env/env.go
@@ -0,0 +1,8 @@
+package env
+
+const (
+	FilePath = "../scraping-school/info.csv"
+	ApiKey   = "AIzaSyDH6Z2D_-J3RFw0x5SYUoCmWe2jXzuHzoU"
+	Cx       = "006284453872434406466:vedpdxtygmy"
+	Query    = "google"
+)
