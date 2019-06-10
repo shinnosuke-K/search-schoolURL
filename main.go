032d5df5d5116a652561bd361cb65dcde6867499
shinnosuke-K <shin.050195@gmail.com>
2019-06-10 14:20:30 +0900

    read csv file and display file Name

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"encoding/csv"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"search-schoolURL/env"
+)
+
 func main() {
+
+	files, err := ioutil.ReadDir(env.DirPath)
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		if fileName := file.Name(); fileName != ".DS_Store" {
+
+			csvFile, err := os.Open(env.DirPath + fileName)
+			if err != nil {
+				panic(err)
+			}
+
+			reader := csv.NewReader(csvFile)
+			var line []string
+
+			for {
+				line, err = reader.Read()
+				if err != nil {
+					break
+				}
+				fmt.Println(line[1])
+			}
+			csvFile.Close()
+		}
+	}
 
 }
